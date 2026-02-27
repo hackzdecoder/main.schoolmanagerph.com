c23c415dab9f177c0876bb88fcfe6fc39c456dbb
masterdev090195 <hackzdecoder@gmail.com>
2026-02-27 16:39:19 +0800

    Test API

--- public/index.php
+++ public/index.php
@@ -8,6 +8,15 @@ define('LARAVEL_START', microtime(true));
 // BLOCK DIRECT ACCESS TO /public
 if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/public/') !== false) {
     http_response_code(500);
+    require __DIR__ . '/../vendor/autoload.php';
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
+    $response->send();
+    $kernel->terminate($request, $response);
+    exit;
 }
 
 // Determine if the application is in maintenance mode...
