3129d3026ee606c103a7c6887d65763fc75caa2b
masterdev090195 <hackzdecoder@gmail.com>
2026-02-27 16:45:21 +0800

    Test API

--- public/index.php
+++ public/index.php
@@ -7,8 +7,21 @@ define('LARAVEL_START', microtime(true));
 
 // BLOCK DIRECT ACCESS TO /public
 if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/public/') !== false) {
-    http_response_code(500);
-    exit('Direct access to /public is forbidden');
+    // http_response_code(500);
+    // exit('Direct access to /public is forbidden');
+
+    require __DIR__ . '/../vendor/autoload.php';
+
+    $app = require_once __DIR__ . '/../bootstrap/app.php';
+
+    $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+    $response = $kernel->handle(
+        $request = Illuminate\Http\Request::capture()
+    );
+    $response->send();
+    $kernel->terminate($request, $response);
+
+    exit;
 }
 
 // Determine if the application is in maintenance mode...
