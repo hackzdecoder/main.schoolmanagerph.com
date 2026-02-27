c87b6c31cceca86b51bff2cb35769ed2bec7b849
masterdev090195 <hackzdecoder@gmail.com>
2026-02-27 16:31:25 +0800

    Test API

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/test-api', function (Request $request) {
     return response()->json([
-        'message' => 'Testing is complete, Do backend now!',
+        'message' => 'Testing is complete, Do now',
     ]);
 });
 
